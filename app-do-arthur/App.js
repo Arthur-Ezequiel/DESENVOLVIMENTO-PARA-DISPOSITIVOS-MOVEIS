7fbc444b6458b46f9d6beeafb1b5876f07b6da6e
Zigsfi <Zigsfi>
2026-08-14 19:37:52 -0300

    Exercicio AULA2

--- app-do-arthur/App.js
+++ app-do-arthur/App.js
@@ -1,18 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SectionList, Button } from 'react-native';
 
 const image = {
   uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrBjz23vepKBCUoK6POM-oLCi70rZwGVVwF40zOIhWwg&s=10'
 };
+
+const dados = [
+  {
+    title: 'Frutas', //TITLE = section
+    data: ['Maça', 'Banana', 'Laranja'] //DATA = item
+  },
+  {
+    title: 'Bebidas',
+    data: ['Agua', 'Suco', 'Refri']
+  }
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
         <Text style={styles.text}>
-          Usei ChatGPT para formatar o código
+          Eu quando... eu quando riu
         </Text>
+
+        <Button 
+        title="Clique aqui" 
+        onPress={() => alert('Se vc clicou aqui, vc é gay')}
+        />
+
+        <SectionList
+  sections={dados}
+  renderItem={({ item }) => <Text>{item}</Text>}
+  renderSectionHeader={({ section }) => (
+    <Text>{section.title}</Text>
+  )}
+/>
       </ImageBackground>
+
+      
     </View>
   );
 }
@@ -28,12 +54,12 @@ const styles = StyleSheet.create({
   },
 
   text: {
-    fontSize: 15,
+    fontSize: 55,
     fontWeight: '500',
     fontStyle: 'normal',
     textDecorationLine: 'underline',
     textAlign: 'center',
-    color: 'pink',
+    color: 'blue',
     backgroundColor: '#000000c0',
   },
 });
